4c175c004cffd08dab6fabcc04a722865fa5fa32
meteofnag <92285840+meteofnag@users.noreply.github.com>
2024-12-25 09:47:20 -0600

    Se registran eventos apendando cadenas de texto

---
+++ bitacora.py
@@ -0,0 +1,8 @@
+
+bitacora = "Inicia proceso"
+bitacora += "\n Inicia etapa 1"
+bitacora += "\n Inicia etapa 2"
+bitacora += "\n Inicia etapa 3"
+bitacora += "\n Inicia etapa final"
+
+print(bitacora)
