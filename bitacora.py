a6e6b9560c852bb206d915b7001294d45ecd8dc2
meteofnag <fnag.conagua@gmail.com>
2025-03-12 00:11:24 -0600

    Obtencion de directorio actual y creacion de carpeta datos

--- bitacora.py
+++ bitacora.py
@@ -1,3 +1,13 @@
+import os
+
+# Obtener el directorio actual
+directorio_actual = os.getcwd()
+print(f"Directorio actual: {directorio_actual}")
+
+# Crear un directorio llamado Datos_PXM
+nuevo_directorio = os.path.join(directorio_actual, "Datos_PXM")
+os.makedirs(nuevo_directorio, exist_ok=True)
+print(f"Directorio creado: {nuevo_directorio}")
 
 bitacora = "Inicia proceso"
 bitacora += "\n Inicia etapa 1"
