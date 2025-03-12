45c97e2926615dc066d1728fd16a1510796a5cd7
meteofnag <fnag.conagua@gmail.com>
2025-03-12 00:23:33 -0600

    Creacion de directorio del dia

--- bitacora.py
+++ bitacora.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 # Obtener el directorio actual
 directorio_actual = os.getcwd()
@@ -8,6 +9,12 @@ print(f"Directorio actual: {directorio_actual}")
 nuevo_directorio = os.path.join(directorio_actual, "Datos_PXM")
 os.makedirs(nuevo_directorio, exist_ok=True)
 print(f"Directorio creado: {nuevo_directorio}")
+
+# Crear una nueva carpeta con la fecha del día actual dentro de Datos_PXM
+fecha_actual = datetime.now().strftime("%Y%m%d")
+directorio_fecha = os.path.join(nuevo_directorio, fecha_actual)
+os.makedirs(directorio_fecha, exist_ok=True)
+print(f"Directorio con fecha creado: {directorio_fecha}")
 
 bitacora = "Inicia proceso"
 bitacora += "\n Inicia etapa 1"
